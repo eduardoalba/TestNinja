87e080f186c6e77798f633e94bd5619f4c0bb180
Eduardo Alba <eduardo.alba@gmail.com>
2019-03-19 15:34:53 -0300

    Add Event test sample

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Internal;
+using System;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -21,12 +22,30 @@ namespace TestNinja.UnitTests
         [TestCase(null)]
         [TestCase("")]
         [TestCase(" ")]
-        public void Log_InvalidValue_ThrowArgumentNullException(string error)
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
         {
             var logger = new ErrorLogger();
 
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
             //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<ArgumentNullException>());
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+
+
+        }
+
+
     }
 }
