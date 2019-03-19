66de780771fd1aa7acfddab6c1def4275a2c9a41
Eduardo Alba <eduardo.alba@gmail.com>
2019-03-19 13:04:27 -0300

    Add void method test sample

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
