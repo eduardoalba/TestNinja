0979cdf1945d003cf2586e055e5377319605dcac
Eduardo Alba <eduardo.alba@gmail.com>
2019-03-20 13:09:15 -0300

    Add FizzBuzz excercise

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(1, "1", TestName = "GetOuput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber")]
+        [TestCase(3, "Fizz", TestName = "GetOuput_InputIsDivisibleBy3Only_ReturnFizz")]
+        [TestCase(5, "Buzz", TestName = "GetOuput_InputIsDivisibleBy5Only_ReturnBuzz")]
+        [TestCase(15, "FizzBuzz", TestName = "GetOuput_InputIsDivisibleBy3And5_ReturnFizzBuzz")]
+        public void GetOuput_WhenCalled_ReturnValue(int value, string exceptedValue)
+        {
+            var result = FizzBuzz.GetOutput(value);
+
+            Assert.That(result, Is.EqualTo(exceptedValue));
+        }
+    }
+}
